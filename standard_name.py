0ab4e2c4baaba6e8f8c71294be8212511b4fa21d
RaviReddy63 <48553316+RaviReddy63@users.noreply.github.com>
2025-07-07 17:05:08 +0530

    Update standard_name.py

--- standard_name.py
+++ standard_name.py
@@ -37,7 +37,7 @@ def standardize_names(df):
                     used_directors.add(other_director)
         
         if len(matching_directors) > 1:
-            longest_name = max(matching_directors, key=len)
+            longest_name = max(matching_directors, key=lambda x: len(x))
             for name in matching_directors:
                 if name != longest_name:
                     director_mapping[name] = longest_name
@@ -68,7 +68,7 @@ def standardize_names(df):
                 used_combinations.add((other_manager, other_director))
         
         if len(matching_managers) > 1:
-            longest_name = max(matching_managers, key=len)
+            longest_name = max(matching_managers, key=lambda x: len(x))
             for name in matching_managers:
                 if name != longest_name:
                     manager_mapping[name] = longest_name
